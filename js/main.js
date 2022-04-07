dab0f81bf1267a72a462aaeec3b3655eb244bf49
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-04-08 00:26:14 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const elItem = document.querySelectorAll(".game__item");
+
+let count = 0;
+elItem.forEach((item) => {
+  item.addEventListener("click", (e) =>{
+    e.target.children[1].style.display = "block";
+    e.target.children[0].style.display = "none";
+    e.target.style.backgroundColor = "#eeaf49";
+    count++;
+    if(count % 2 == 1){
+      e.target.children[0].style.display = "block";
+      e.target.children[1].style.display = "none";
+      e.target.style.backgroundColor = "#dd6859";
+    }
+  });
+});
+
